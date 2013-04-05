969d095cd5482dc4b233150638de6a1faf66ea24
nebgnahz <nebgnahz@gmail.com>
2013-04-05 16:21:01 -0700

    added arguments for script

--- server/wireless.py
+++ server/wireless.py
@@ -1,8 +1,23 @@
-import serial, glob
+import serial, glob, argparse
+
+parser = argparse.ArgumentParser(description='Pyserial for XBee')
+parser.add_argument('--debug', default=False, action='store_true', help='Print More Errors and Launch interactive console on exception or forced exit')
+parser.add_argument('--baud', type=int, action='store', default=9600, help='Specify the baud rate')
+parser.add_argument('--timeout', type=float, action='store', default=1, help='Timeout parameter for serial connection')
 
 # we know for mac it will show as /dev/usb.tty*, so list all of them and ask user to choose
 
 availables = glob.glob('/dev/tty.usb*')
+
+arguments = parser.parse_args()
+try:
+	baud = arguments.baud
+	timeout = arguments.timeout
+	print "  baud rate:", baud
+	print "  serial timeout:", timeout
+except Exception as e:
+	print e
+	exit(1)
 
 print "All available ports:" 
 
@@ -13,20 +28,21 @@ for port in availables:
 
 try:
 	num = int(raw_input('Enter the number:'))
-	if num > 0 and num < len(availables):
+	if num > 0 and num <= len(availables):
 		selected = availables[num-1]
-except:
-	print 'invalid input!'
+except Exception as e:
+	print "invalid input!"
+	exit(1)
+
 
 try:
-	baud = int(raw_input('Baud rate (9600):'))
-except:
-	baud = 9600
-
-
-ser = serial.Serial(selected, baud)
+	ser = serial.Serial(selected, baud, timeout=timeout)
+except Exception as e:
+  print "failed to connect the serial port", e
+	
 while True:
-	print ser.read()
+	ser.write("testing")
+	print ser.readline()
 
 
 
