89ae5b8f8d7ce1b3ef136958dc10160434d9474b
Sean Chen <sean.yhc@gmail.com>
2013-04-04 23:46:30 -0700

    add switch clause to determine received IR signals
    currently using some random Sony buttons, can be changed later

--- XBee_test/XBee_test.ino
+++ XBee_test/XBee_test.ino
@@ -6,6 +6,17 @@ int ledPin = 13;
 int RECV_PIN = 8;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
+int deviceStatus = 0;
+int pendingTimer;
+
+#define statusOff 0
+#define statusPending 1
+#define statusOn 2
+
+
+const unsigned long signalInit = 0xA90;
+const unsigned long signalVerify = 0xAF0;
+const unsigned long signalConfirm = 0xA70;
 
 void setup()  
 {
@@ -21,7 +32,7 @@ void setup()
 
 void loop() // run over and over
 {
-  if (XBee.available()) {
+  if(XBee.available()) {
     Serial.write(XBee.read());
     
     digitalWrite(ledPin, HIGH);
@@ -30,16 +41,46 @@ void loop() // run over and over
     XBee.write("roger");
   }
   
-  if (Serial.available()) {
+  if(Serial.available()) {
     Serial.write(Serial.read());
     XBee.println("msg from serial");
   }
   
-  if (irrecv.decode(&results)) {
+  if(irrecv.decode(&results)) {
+    
+    //todo: filter redundant FFFFFFFF
+
     Serial.println(results.value, HEX);
+    // Serial.println(results.decode_type);
+    
     XBee.print("IR: ");
     XBee.println(results.value, HEX);
+
+    switch(results.value) {
+      case signalInit:
+        Serial.println("singalStart");
+        XBee.println("singalStart");
+        break;
+      
+      case signalVerify:
+        Serial.println("singalVerify");
+        XBee.println("singalVerify");
+        break;
+
+      case signalConfirm:
+        Serial.println("singalConfirm");
+        XBee.println("singalConfirm");
+        break;
+
+    }
+
     irrecv.resume(); // Receive the next value
+
+  }
+  
+  if(deviceStatus == statusPending) {
+    //todo: start blinking
+    ;
   }
     
 }
