b40b9d869ac73ce7d08a2a02966440709a8ae7d3
nebgnahz <nebgnahz@gmail.com>
2013-04-08 18:10:56 -0700

    added serial-monitor script since there is conflict when I debug for two connected Arduinos; added client code, which is still underdevelopment with the glass code

---
+++ client/client.ino
@@ -0,0 +1,136 @@
+#include <SoftwareSerial.h>
+#include <IRremote.h>
+
+// Trying to elucidate the way communication works first
+// current design enforces a simplest design on the receiver side
+// no scheduling, purely acknowledge everything it has received with a random delay
+#define IDLE 0
+#define PENDING 1
+#define CONNECTED 2
+
+SoftwareSerial XBee(2, 3); // RX, TX
+int ledPin = 13;
+int RECV_PIN = 8;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+int deviceStatus = 0;
+int pendingTimer;
+int deviceId = 1;
+char XBeeInString[50];
+int state = IDLE;
+unsigned long randomDelay = 0;
+
+#define statusOff 0
+#define statusPending 1
+#define statusOn 2
+
+struct XBeeMessage {
+  int deviceId;
+  unsigned long msg;
+};
+
+// const unsigned long signalInit = 0xA90;
+const unsigned long signalInit = 0xC1AA09F6;
+const unsigned long signalVerify = 0xAF0;
+const unsigned long signalConfirm = 0xA70;
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);   
+  Serial.println("system begins!");
+
+  // set the data rate for the SoftwareSerial port
+  XBee.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+
+  randomSeed(analogRead(5));
+}
+
+int tmpTest = 0;
+
+void loop()
+{
+  switch(state) {
+  case IDLE:
+    // purely listening and then react by response
+    // for now test use random
+    // if(irrecv.decode(&results)) {
+    tmpTest = random(100);
+    Serial.print("[IDLE] tmp:");
+    Serial.println(tmpTest);
+    if (tmpTest > 90) {
+      randomDelay = random(100);
+      delay(randomDelay);
+
+      Serial.print(deviceId);
+      Serial.print(": ");
+      Serial.print(tmpTest);
+      Serial.println();
+
+      XBee.print(deviceId);
+      XBee.print(": ");
+      XBee.print(tmpTest);
+      XBee.println();
+    }
+    break;
+  case PENDING:
+    if(XBee.available()) {
+      Serial.print(XBee.read());
+      return;
+      
+      // determine if the designated id is the deviceId
+      // read XBee message first
+      if (1 == deviceId) {
+	// have been confirmed
+	state = CONNECTED;
+      }
+      else {
+	state = IDLE;
+      }
+    }
+    if(irrecv.decode(&results)) {
+      randomDelay = random(100);
+      delay(randomDelay);
+      XBee.print(deviceId);
+      XBee.print(":");
+      XBee.println(results.value, HEX);
+      state = PENDING;
+    }
+    break;
+  case CONNECTED:
+    // pass
+    break;
+  default:
+    break;
+  }
+  
+}
+
+void sendMsg(String msg) {
+  Serial.print(deviceId);
+  Serial.print(": ");
+  Serial.print(msg);
+  Serial.println();
+
+  XBee.print(deviceId);
+  XBee.print(": ");
+  XBee.print(msg);
+  XBee.println();
+
+}
+
+void readXBeeString (char *strArray) {
+  int i = 0;
+  if(!XBee.available()) {
+    return;
+  }
+  while (XBee.available()) {
+    strArray[i] = XBee.read();
+    i++;
+  }
+  Serial.print("read XBee: ");
+  Serial.println(strArray);
+  // sendMsg(XBeeInString);
+}
--- glass/glass.ino
+++ glass/glass.ino
@@ -31,14 +31,15 @@ unsigned long new_time;
 struct XBeeMessage XBeeMsgArr[20];
 int XBeeMsgPointer;
 int selectedXBee;
-
+  
 void setup()
 {
   Serial.begin(9600);
-  state = IDLE;
+  state = INIT;
   digitalWrite(softpotPin, HIGH); //enable pullup resistor
   randomSeed(analogRead(5));
   digitalWrite(ledPin, LOW);
+  XBee.begin(9600);
   XBeeMsgPointer = 0;
 }
 
@@ -47,48 +48,87 @@ void loop() {
   case IDLE:
     softpotReading = analogRead(softpotPin);
     Serial.print("Softpot Reading: ");
-    Serial.println(softpotReading);    
+    Serial.println(softpotReading);
+    delay(20);
     if (softpotReading < SOFTPOT_THREASHOLD) {
       state = INIT;
     }
     selectedXBee = -1;
     break;
   case INIT:
+    Serial.println("[INIT] Sending IR");
     session_id = random(0xFFFF);
     irsend.sendSony(session_id, 16);
     // at the same time listening to any response
     if(XBee.available()) {
       state = WAIT;
       start_time = millis();
+      Serial.print("[WAIT] entering state, time=");
+      Serial.println(start_time);
       XBeeMsgPointer = 0;
     }
+    // this delay is purely for rate limiting now
+    delay(30);
     break;
   case WAIT:
     // When entering this state, wait for 1 sec to determine how many signals have been received
     new_time = millis() - start_time;
     if (new_time > 1000) {
-      // enter to a new state based on counts
-      if (XBeeMsgPointer == 1) {
+      Serial.print("[WAIT] One sec expires, client count:  ");      
+      Serial.println(XBeeMsgPointer);
+      if (XBeeMsgPointer == 0) {
+	// 
+      }
+      else if (XBeeMsgPointer == 1) {
 	// only one responded, cool, just go to confirm stage
+	Serial.println("[CONFIRM] entering");
 	state = CONFIRM;
 	selectedXBee = XBeeMsgPointer - 1;
       }
       else {
 	state = VERIFY;
       }
-    }      
+    }
     if(XBee.available()) {
-      switch(XBee.read()) {
+      char packet[50];
+      char id[10];
+      char msg[40];
+      int i = 0;
+      char *str;
+      char *p = packet;
+      
+      // obtain the packet
+      while (XBee.available()) {
+	packet[i++] = (char) XBee.read();	
+	// Serial.print( (char) ( XBee.read()) );
+      }
+      packet[i] = '\0';
 
-      // here need debugging to see what is the message like
-      case 0: // "success"
-	// XBeeMsgArr[XBeeMsgPointer].deviceId = id;
-	// XBeeMsgArr[XBeeMsgPointer].msg = msg;
+      // a weak design, you should be cautious when packets are corrupted
+      str = strtok_r(p, ":", &p);
+      strcpy(id, str);
+      str = strtok_r(p, ":", &p);
+      strcpy(msg, str);
+
+      // assert( (str = strtok_r(p, ":", &p)) != NULL);
+      // for debugging
+      Serial.println("[WAIT] Get XBee reading:");
+      Serial.print("id:");
+      Serial.print(id);
+      Serial.print(" msg:");
+      Serial.println(msg);
+      
+      for(i = 0; i <= XBeeMsgPointer; i++) {
+	if (XBeeMsgArr[i].deviceId == atoi(id))
+	  break;
+      }
+      if (i > XBeeMsgPointer) {
+	// not found, insert it
+	XBeeMsgArr[XBeeMsgPointer].deviceId = atoi(id);
+	XBeeMsgArr[XBeeMsgPointer].msg = atoi(msg);
 	XBeeMsgPointer++;
 	break;
-      default:
-	break;
-      }
+      }      
     }
     break;
   case CONFIRM:
@@ -111,5 +151,5 @@ void loop() {
     break;
   default:
     break;
-  }  
+  }
 }
