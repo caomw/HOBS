150ef71cb2b6f5f94f280bf087cb3904300fe9a4
nebgnahz <nebgnahz@gmail.com>
2013-07-25 14:09:01 -0700

    added IR test, for mega, use pin 9

---
+++ test/IR_test/IR_test.ino
@@ -0,0 +1,33 @@
+/*
+  IR test
+
+  Used to test IR functionality.
+
+  On Mega, you need to use pin 9
+  
+  Created 07/25/2013
+  By benzh@eecs.berkeley.edu
+
+*/
+
+#include <IRremote.h>
+
+// An IR LED must be connected to Arduino PWM pin 3.
+// On Mega, you need to use pin 9
+IRsend irsend;
+int ledPin = 8;
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(100);
+  Serial.print("system begins!");
+}
+
+void loop() {
+  delay(1000);
+  unsigned int session_id = random(0xFFFF);
+  Serial.print("[INIT] Sending IR: ");
+  Serial.println(session_id);
+  irsend.sendSony(session_id, 16);
+}
