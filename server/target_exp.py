c511d14b2b1a17483213dcb56e794a954068cb30
seanyhc <sean.yhc@gmail.com>
2013-09-12 15:29:27 -0700

    refine catching success msg out of a cluster

--- server/target_exp.py
+++ server/target_exp.py
@@ -27,13 +27,14 @@ start_time = time.time()
 
 target_list = []
 # predefined_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
-predefined_list = ['08', '02', '04', '01', '05', '10', '07', '03', '09', '06', '01', '09', '02', '10', '07']
-total_client_no = 4
-total_task_round = 6
+predefined_list = ['08', '02', '04', '01', '05', '10', '07', '03', '09', '06', '05', '09', '02', '06', '07']
+trial_list = ['01', '04', '09']
+total_client_no = 10
+total_task_round = 15
 list_cursor = 0
 task_interval = 3  # after selecting a correct candidate, wait for couple seconds till next target shows
 state_cue = True
-correct_target = False
+success = 0;
 
 MODE_PREDEFINE = 1
 MODE_RANDOM = 2
@@ -43,7 +44,7 @@ exp_mode = 0
 def logResult(action, obj):
   ts = time.time()
   global start_time
-  if action == 'target':
+  if action == 'start':
     # reset start time everytime a target is sent
     start_time = ts
   tup = (ts-start_time, action, obj)
@@ -61,13 +62,15 @@ def prepare_target_list(type):
       result.append("%02d" % t)
   elif type == 3:
     restult = []
+  elif type == 4:
+    result = trial_list
   print 'list: ', result
   return result
 
 def end_exp():
   print 'writing results'
   global user_id, result_log, ser
-  f = open("result_"+user_id[:-1]+".txt", 'w')
+  f = open("../data/result_"+user_id[:-1]+".txt", 'w')
   for t in result_log:
     print t
     f.write(' '.join(str(s) for s in t) + '\n') 
@@ -79,11 +82,10 @@ def end_exp():
 def send_cue(tid):
   # print "debug:", tid
   out = tid + "CSELTAR"
-  logResult("target", tid)
+  # logResult("target", tid)
   global list_cursor
-  print 'round', list_cursor+1
-  print 'Target msg:', out
-  print 'out length:', len(out)
+  print '=== round', list_cursor+1, '/', len(target_list), 'success:', success, '==='
+  print 'Target msg:', out, 'length:', len(out)
   return out
 
 availables = glob.glob('/dev/tty.*')
@@ -128,21 +130,23 @@ user_id = sys.stdin.readline()
 print "1. auto from predefined list"
 print "2. auto from random"
 print "3. manually enter (e.g. T01)"
+print "4. trial test"
 
 try:
   num = int(raw_input('Enter the number:'))
-  if num > 0 and num <= 3:
+  if num > 0 and num <= 4:
     exp_mode = num
     target_list = prepare_target_list(exp_mode)
   else:
     print 'out of range'
     exit(1)
 except Exception as e:
-  print "invalid input!"
+  print "invalid mode!"
   exit(1)
 
 while True:
   if state_cue == True:
+    
     if exp_mode == MODE_MANUAL:
     # read line without blocking
       # while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
@@ -176,25 +180,35 @@ while True:
         ser.write(cmd)
     else: # predefined or random list
       if list_cursor > len(target_list) - 1:
-        print 'exp done'
+        print '=== Exp done ==='
         end_exp()
         exit(0)
       target_id = target_list[list_cursor]
-      state_cue = False
       out = send_cue(target_id)
       ser.write(out)
+      state_cue = False
   
   # can reset or end exp within a task phase
   while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
     line = sys.stdin.readline()
-    print '[Console]: ',line
-    print 'line length: ',len(line)
-    if line[0:5] == 'reset':
+    print '[Console]: ',line, 'length: ',len(line)
+    if line[0:3] == 'res':
       # repeat the cue stage (if cue ack isn't received)
-      list_cursor -= 1
       state_cue = True
+      logResult("reset", target_id)
     elif line[0:3] == "end":
       end_exp()
+      logResult("end", "")
+    elif line[0:4] == "next":
+      state_cue = True
+      target_list.append(target_id)
+      print target_list
+      list_cursor += 1
+      logResult("next", "")
+    elif line[0:4] == "skip":
+      state_cue = True
+      list_cursor += 1
+      logResult("skip", "")
     else:
       print 'writing cmd to XBee'
       ser.write(line)  
@@ -207,26 +221,34 @@ while True:
       logResult("connect", '')
     elif in_msg[2:9] == "ASELTAR":
       # is ack for target cue, record timestamp
-      logResult("cue shown", in_msg[0:2])
-      list_cursor += 1
       state_cue = False
+      ser.write("XXSSELOFF")
+      print '=== next target: ===', target_list[list_cursor]
+      cmd = raw_input('=== press Enter to proceed to next target ===')  
+      logResult("start", in_msg[0:2])
+    elif "SELAON" in in_msg:
+      i = in_msg.find('SELAON')
+      if in_msg[i-3:i-1] == target_id:
+        logResult("correct_single", target_id)
+        list_cursor += 1
+        success += 1
+      else:
+        logResult("wrong_single", in_msg[0:2])
+    elif "SEL ON" in in_msg:
+      # a client is selected, check result
+      i = in_msg.find('SEL ON')
+      if in_msg[i-3:i-1] == target_id:
+        logResult("correct_mul", target_id)
+        list_cursor += 1
+        success += 1
+      else:
+        logResult("wrong_mul", in_msg[0:2])  
     elif in_msg[3:9] == "SEL1st":
       logResult("multiple", in_msg[0:2])
     elif in_msg[3:9] == "SEL NA":
       logResult("miss", '')
     elif in_msg[3:9] == "SEL080":
       logResult("switch", in_msg[0:2])
-    elif in_msg[3:9] == "SELAON":
-      if in_msg[0:2] == target_id:
-        logResult("correct_single", target_id)
-      else:
-        logResult("wrong_single", in_msg[0:2])
-    elif in_msg[3:9] == "SEL ON":
-      # a client is selected, check result
-      if in_msg[0:2] == target_id:
-        logResult("correct_mul", target_id)
-      else:
-        logResult("wrong_mul", in_msg[0:2])
     elif in_msg[3:9] == "SELCAN":
       logResult("cancel_mul", '')
     elif in_msg[3:9] == "SELOFF":
@@ -235,8 +257,8 @@ while True:
         logResult("disconnected_right", target_id)
         target_id = "NA"
         state_cue = True
-        print 'waiting', task_interval, 'sec to cue next target'
-        sleep(task_interval)  # wait 3s to cue next target
+        #print 'waiting', task_interval, 'sec to cue next target'
+        # sleep(task_interval)  # wait 3s to cue next target
       else:
         logResult("disconnected_wrong", target_id)
 
